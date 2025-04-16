843e948a3875cf70e6af5d008690994174f4fe8c
CyberCoder08 <yashchoubey31@gmail.com>
2025-04-16 20:40:57 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
   const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=${per_page}`, {
     headers: {
-      Authorization: process.env.tPlJc5tScaNJbJtYPLIW5CvCb9uDHaGU4z4Drocl45QlKU7kLfyJl5hl,
+      Authorization: process.env.8cBUjJ99dXuFDzwqUgvMOyzHUd5hLCz3mYBRZoCUfI8,
     },
   });
 
