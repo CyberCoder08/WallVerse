78e20dfda1986e4ea9ecccaeb1dd8ed21e3c8709
CyberCoder08 <yashchoubey31@gmail.com>
2025-04-16 20:03:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const API_KEY = "tPlJc5tScaNJbJtYPLiW5CvCb9uDHaGU4z4DrocI45QlKU7kLfyJl5hl";
 const grid = document.getElementById("wallpaperGrid");
 
 async function searchWallpapers() {
   const query = document.getElementById("searchInput").value;
-  const url = `https://api.pexels.com/v1/search?query=${query}&per_page=30`;
+  const url = `/api/search?query=${encodeURIComponent(query)}&per_page=30`;
 
-  const res = await fetch(url, {
-    headers: {
-      Authorization: API_KEY
-    }
-  });
-
+  const res = await fetch(url);
   const data = await res.json();
   displayWallpapers(data.photos);
 }
